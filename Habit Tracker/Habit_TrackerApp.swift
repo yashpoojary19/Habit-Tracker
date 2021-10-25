47895a89da5d63c7f541be871f8a32b7f279c680
Yash Poojary <yashspoojary245@gmail.com>
2021-10-25 17:39:22 +0530

    Initial Commit

---
+++ Habit Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit Tracker
+//
+//  Created by Yash Poojary on 25/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
